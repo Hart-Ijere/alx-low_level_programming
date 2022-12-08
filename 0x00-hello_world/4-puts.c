446bc829654f5df7c962f70a0d8575ce6eed09cb
Hart-Ijere <iyeowayi@gmail.com>
2022-12-08 08:17:56 -0800

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
-#include <stdio.h>                                                                                                        
+#include <stdio.h>
 
-                                                                                                                          
+/**
+*main - Entry point
+*Return 0 (success)
+*/
 
-/**                                                                                                                       
-
-*main - Entry point                                                                                                       
-
-*Return: Always 0 (success)                                                                                               
-
-*/                                                                                                                        
-
-                                                                                                                          
-
-int main(void)                                                                                                            
-
-{                                                                                                                         
-
-puts("programming is like building a multilingual puzzle");                                                               
-
+int main(void)
+{
+puts("programming is like building a multilingual puzzle");
 return (0);
+}
