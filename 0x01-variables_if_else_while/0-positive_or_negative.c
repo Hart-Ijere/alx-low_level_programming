896e0bbcf73b395f5145883809eda9db21c508ea
Hart-Ijere <iyeowayi@gmail.com>
2022-12-12 10:46:15 -0800

    assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,38 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 /* more headers goes there */
 
-/* betty style doc for main goes there */
-int main ()
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
 {
+
   int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX /2;
-  /* Your code goes there */
-  if (n > 0) {
-    printf("%d\n" . n "is a positive number");
-  }else if(n == 0)
-    {
-      printf("%d\n" . n "is zero");
-    }
-  else{
-    printf("%d\n" . n "is negative");
-  }
-  return (0);
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	  {
+	    printf("%d is positive\n", n);
+	  }
+	else if (n == 0)
+	  {
+	    printf("%d is zero\n", n);
+	  }
+	else
+	  {
+	    printf("%d is negative\n", n);
+	  }
+	
+	return (0);
 
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
-Assign random number to variable n
+0x01. C - Variables, if, else, while
+Contains projects on variables, if, else and while.
