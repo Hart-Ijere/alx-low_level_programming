4dc32b59f5ac124ab3d5ef3c5bb9baeb3308a2f3
Hart-Ijere <iyeowayi@gmail.com>
2023-11-21 02:56:20 +0100

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string followed by new line.
+ * @s: parameter
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+  if (*s == '\0')
+  {
+    return;
+  }
+  putchar(*s);
+  _puts_recursion(s + 1);
+ 
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This Directory contains alx Recursion Projects
