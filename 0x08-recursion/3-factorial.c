bc36c9ebb9d1a093b0f45b54281a4869c7817602
Hart-Ijere <iyeowayi@gmail.com>
2023-11-21 04:49:12 +0100

    More modification of 3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * 3-factorial - funcrion that prints the factori * al of a given number
- * @n: the given number
- *
- * Return: -1 if n is lower than 0.
- */
+* factorial - calculates the factorial of a numb* er
+* @n: number to be evaluated
+*
+* Return: (-1) if n is less than zero
+*/
 
 int factorial(int n)
 {
-if (n < 0)
-return (-1);
+  if (n < 0)
+    return (-1);
 
-if (n == 0)
-return (1);
+  if (n == 0)
+    return (1);
 
-return n * factorial(n - 1);
-
+  return n * factorial(n - 1);
 }
 
+    
 
