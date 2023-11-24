0ccdba49e9e244917db6d48312ee408a1fdf4a2e
Hart-Ijere <iyeowayi@gmail.com>
2023-11-24 14:32:06 +0100

    modify

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,13 +11,10 @@
 int factorial(int n)
 {
 if (n < 0)
-return (-1); 
+return (-1);
 
-if  (n == 0) 
-return (1); 
+if (n == 0)
+return (1);
 
-return n * factorial(n - 1);
+return (n * factorial(n - 1);
 }
-
-
-
