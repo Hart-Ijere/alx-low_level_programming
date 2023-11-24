166ff7324bba4181108be09f3444b125f1159a40
Hart-Ijere <iyeowayi@gmail.com>
2023-11-24 17:57:54 +0100

    modify for betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * factorial - function that returns the factoria
+ * l of a given number
+ * @n: the given number
+ *
+ * Return: return (-1) if n is lower than zero
+ */
+
 int factorial(int n)
 {
 if (n < 0)
@@ -11,6 +19,5 @@ if (n == 0)
 {
 return (1);
 }
- return (n * factorial(n - 1));
+return (n * factorial(n - 1));
 }
-
