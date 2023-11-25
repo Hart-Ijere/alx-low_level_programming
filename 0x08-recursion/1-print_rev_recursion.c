9d98cf43fd735bd89548b3508d6305f3b137d13c
Hart-Ijere <iyeowayi@gmail.com>
2023-11-25 03:27:36 +0100

    print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _print_rev_recursion - function that prints a st* ring in reverse
+* @s: character array to be printed
+*
+* Return: Nothing
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+
+return (_print_rev_recursion(s + 1));
+putchar(*s);
+}
