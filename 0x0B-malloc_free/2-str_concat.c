85ffc2aa1fab38e55f73295ac39dcedf8ee7317e
Hart-Ijere <iyeowayi@gmail.com>
2023-12-06 04:05:01 +0100

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
 /*
  * Concatenates two strings.
@@ -17,7 +18,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *result:
+char *result;
 if (s1 == NULL)
 {
 s1 = "";
