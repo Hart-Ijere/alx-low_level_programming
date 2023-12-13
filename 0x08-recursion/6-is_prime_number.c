9d22ef61959c9b08ff03af5d5cecfd6a8abf2d9b
Hart-Ijere <iyeowayi@gmail.com>
2023-12-13 08:20:51 +0100

    check for a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,10 +6,16 @@
  * er is a prime number
  * @n: number to be checked
  *
- * is_prime_number_hp - helper
- * @i - parameter
  * Return: 1 if number is a prime, 0 otherw
  * ise
+ */
+
+/**
+ * is_prime_number_hp - helper function
+ * @n - parameter
+ * @i - parameter
+ *
+ * Return: 1
  */
 
 int is_prime_number_hp(int n, int i)
@@ -24,7 +30,7 @@ return (1);
 }
 if (n % i == 0)
 {
-return 0;
+return (0);
 }
 return (is_prime_number_hp(n, i - 1));
 }
